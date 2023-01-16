9b01106365b474369cc1c71fd4c916755a79fa1d
Pena <gui.domenes@gmail.com>
2023-01-16 18:40:45 -0300

    atual

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require("express")
 const app = express()
+const cors = require('cors')
+app.use(cors())
+
 const port = process.env.PORT || 3000
 
 const produtos = require('./src/produtos/produtos.json')
 
 app.get('/produtos', (req,res) => {
     return res.json(produtos)
+})
+
+app.delete('/produtos/:id', (req,res) => {
+    var id = req.params.id
+    console.log(id);
 })
 
 app.listen(port, () => {
